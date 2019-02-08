0f7973ae450d431b0e63a815b7740f313f529d09
VladimirZhdanov <VladimirZhdanov1992@gmail.com>
2019-02-08 21:04:04 +0600

    Saving avatars

---
+++ src/handlers.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const crypto = require("crypto");
+const fs = require("fs");
+const getStream = require("get-stream");
+//
+const cfg = require("../config.json");
+//
+const VALID_EXTENSIONS = [".jpg", ".jpeg", ".png", ".svg"];
+
+const sendJsonResponse = (res, status, data, code = 200, error = null) => {
+    res.status = status;
+    res.json({
+        status,
+        code,
+        data,
+        error
+    });
+};
+
+const upload_handler = (req, res) => {
+    if (req.headers["x-filename"]) {
+        const ext = path.extname(req.headers["x-filename"]).toLowerCase();
+        // Check extension
+        if (!VALID_EXTENSIONS.includes(ext)) {
+            sendJsonResponse(
+                res,
+                "error",
+                "",
+                400,
+                "Invalid filename extension"
+            );
+            return;
+        }
+
+        //
+        getStream.buffer(req).then(data => {
+            const hasher = crypto.createHash("md5");
+            // create hash
+            hasher.write(data);
+            const hash = hasher.digest("hex");
+            // save file
+            const filename = `${hash}${ext}`;
+            const file_path = `${cfg.media_path}/${filename}`;
+            const url = `${cfg.media_url}/${filename}`;
+            const file = fs.createWriteStream(file_path);
+            file.write(data);
+            sendJsonResponse(res, "success", url);
+        });
+
+        // req.on("end", () => {
+        //     const hash = hasher.digest("hex");
+        //     const filename = `${hash}${ext}`;
+        //     const file_path = `${cfg.media_path}/${filename}`;
+        //     const url = `${cfg.media_url}/${filename}`;
+
+        //     req.pipe(fs.createWriteStream(file_path));
+        //     console.log("HELO!");
+        //     req.on("end", () => {
+        //         sendJsonResponse(res, "success", url);
+        //     });
+        // });
+
+        // Save data as file in ./media
+    } else {
+        sendJsonResponse(
+            res,
+            "error",
+            "",
+            400,
+            "Header X-Filename is required!"
+        );
+    }
+};
+module.exports = {
+    upload_handler
+};
--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const path = require("path");
 const expressLogging = require("express-logging");
 const logger = require("logops");
-const fs = require("fs");
 const io = require("socket.io");
+const bodyParser = require("body-parser");
+//
 const cfg = require("../config.json");
 const Room = require("./Room");
 const User = require("./User");
 const Message = require("./Message");
+const { upload_handler } = require("./handlers");
 
 // loglevel.setLevel()
-
-const VALID_EXTENSIONS = [".jpg", ".jpeg", ".png", ".svg"];
 
 module.exports = () => {
     // create server
@@ -19,29 +18,11 @@ module.exports = () => {
     const app = express();
     const ios = io.listen(app.listen(cfg.port, cfg.host));
     const room = new Room(ios);
-
     app.use(expressLogging(logger));
+    app.use(bodyParser.raw());
+    app.use("/media", express.static("media"));
 
-    app.post("/upload", (req, res) => {
-        if (req.headers["x-filename"]) {
-            const ext = path.extname(req.headers["x-filename"]).toLowerCase();
-            if (!VALID_EXTENSIONS.includes(ext)) {
-                res.status(400);
-                res.end("Invalid filename extension");
-            } else {
-                req.pipe(
-                    fs.createWriteStream(`./media/${Math.random()}${ext}`)
-                );
-                req.on("end", () => {
-                    res.end("Good file!");
-                });
-            }
-        } else {
-            res.status(400);
-            res.write("Hello\n");
-            res.end("Header X-Filename is required!");
-        }
-    });
+    app.post("/upload", upload_handler);
 
     ios.on("connection", socket => {
         socket.on("AUTH", data => {
